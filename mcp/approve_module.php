ef9f7282ab577b5b46b5ed7e20872e483b20d683
David Colón <david@davidiq.com>
2023-07-15 18:20:18 -0400

    feat(Approve): Add placeholders for MCP approve module

--- mcp/approve_module.php
+++ mcp/approve_module.php
@@ -18,17 +18,41 @@ class approve_module
 	var $u_action;
 
 	/**
+	 * @var string
+	 */
+	var $tpl_name;
+
+	/**
+	 * @var string
+	 */
+	var $page_title;
+
+	/**
 	 * @param $id
 	 * @param $mode
 	 */
 	function main($id, $mode)
 	{
-		global $template, $user;
+		global $phpbb_container;
 
-		$this->tpl_name = 'mcp_demo_body';
-		$this->page_title = $user->lang('MCP_DEMO_TITLE');
-		add_form_key('acme/demo');
+		$lang = $phpbb_container->get('language');
+		$request = $phpbb_container->get('request');
+		$mcp_controller = $phpbb_container->get('mrgoldy.ultimateblog.controller.mcp');
+		$mcp_controller->set_page_url($this->u_action);
+		
+		switch ($mode)
+		{
+			case 'ub_blog_approve':
+				$this->tpl_name = 'ub_mcp_approve_body';
+				$this->page_title = $lang->lang('MCP_UB_BLOG_QUEUE');
+				$mcp_controller->unapproved_blogs();
+				break;
 
-		$template->assign_var('U_POST_ACTION', $this->u_action);
+			case 'ub_comment_approve':
+				$this->tpl_name = 'ub_mcp_approve_body';
+				$this->page_title = $lang->lang('MCP_UB_COMMENT_QUEUE');
+				$mcp_controller->unapproved_comments();
+				break;
+		}
 	}
 }
