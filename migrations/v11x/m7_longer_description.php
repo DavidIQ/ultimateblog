7278aabd25294cae12b5fb15e88bf71b3adc6602
David Colón <david@davidiq.com>
2023-07-16 07:18:57 -0400

    feat(Description): Add migration to expand decription column

---
+++ migrations/v11x/m7_longer_description.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ *
+ * Ultimate Blog. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2023, DavidIQ
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace mrgoldy\ultimateblog\migrations\v10x;
+
+/**
+ * Class m6_permissions
+ *
+ * @package mrgoldy\ultimateblog\migrations\v10x
+ */
+class m6_permissions extends \phpbb\db\migration\container_aware_migration
+{
+	/**
+	 * @return void
+	 * @access public
+	 */
+	static public function depends_on()
+	{
+		return array('\mrgoldy\ultimateblog\migrations\v10x\m1_initial_schema');
+	}
+
+	/**
+	 * @return void
+	 * @access public
+	 */
+	public function update_schema()
+	{
+		return [
+			'change_columns'		=> [
+				$this->table_prefix . 'ub_blogs'	=> [
+					'blog_description'		=> array('VCHAR:325', ''),
+				],
+			],
+		];
+	}
+
+	/**
+	 * @return void
+	 * @access public
+	 */
+	public function revert_schema()
+	{
+		return [
+			'change_columns'		=> [
+				$this->table_prefix . 'ub_blogs'	=> [
+					'blog_description'		=> array('VCHAR:125', ''),
+				],
+			],
+		];
+	}
+}
